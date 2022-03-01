27757e10910eb01bc2f2ed3d6a2a131aaadfd06f
Cat Kutay <cat.kutay@cdu.edu.au>
2022-03-01 16:27:29 +0930

    fixed apk

--- LivingArchive/home/models.py
+++ LivingArchive/home/models.py
@@ -3,33 +3,20 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
-from LivingArchive.settings.dev import API_KEY
-import os
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'WARNING',
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
-            'propagate': False,
-        },
-    },
-}
+from djangokeys import DjangoKeys
+keys=DjangoKeys ("./LivingArchive/settings/.env")
+
+
+import logging
+logger = logging.getLogger(__name__)
+
 class HomePage(Page):
+    
     body = RichTextField(blank=True)
     #get data from file dev.py and pass to interface
-    api_key = API_KEY,
+    api_key = keys.str("API_KEY")
+    #logging.warn("Api_Key", API_KEY,)
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
     ]
