ffe534a035e568aeb90b894fc98f747a80b07491
root <root@studentlearn1.cdu.edu.au>
2023-11-07 21:55:03 +0000

    Updating dataabase

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -2,7 +2,7 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
-import wagtail.core.fields
+import wagtail.fields
 
 
 class Migration(migrations.Migration):
@@ -31,7 +31,7 @@ class Migration(migrations.Migration):
                 ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.page')),
                 ('date', models.DateField(verbose_name='Post date')),
                 ('intro', models.CharField(max_length=250)),
-                ('body', wagtail.core.fields.RichTextField(blank=True)),
+                ('body', wagtail.fields.RichTextField(blank=True)),
                 ('address', models.CharField(max_length=255, null=True)),
                 ('blog_image', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='wagtailimages.image')),
             ],
